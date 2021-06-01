9bc29de9348cdb56fcb50deea96cfeb783516372
vrlx3 <bhavsar.viral@outlook.com>
2021-06-01 15:25:01 -0500

    Update reguser.js

--- src/components/reguser.js
+++ src/components/reguser.js
@@ -1,7 +1,11 @@
+import React, {useState} from 'react'
+
 
 const newuser = () => {
     //extract info from form below and run 
-    const x = bkfhgihgl
+   
+
+
     // username & password
 
 
@@ -10,20 +14,31 @@ const newuser = () => {
     // Welcome mr. superman
 
     console.log('new user register attempt')
+    // console.log('username ', username)
 }
 
 const Reguser = () => {
 
+    const [username, setUsername] = useState('')
+    const [password, setPassword] = useState('')
+
     return (<div id='reguser'>
-            <input type='form' placeholder='Choose Username'></input>
-            <p></p>
-            <input type='form' placeholder='Choose Password'></input>
-            <button onClick={newuser}>Register</button>
+            <form onSubmit={ () => {
+                
+                }
             
-
+            }>
+            <input id='username' type='text' placeholder='Choose Username' onChange={ (e) => {setUsername(e.target.value)}}></input>
+            <p></p>
+            <input id='password' type='text' placeholder='Choose Password' onChange={ (e) => {setPassword(e.target.value)}}></input>
+            <p></p>
+            <button type='submit' >Register</button>
+            </form>
             </div>
-
+            
     )
+
+
 }
 
 export default Reguser;
