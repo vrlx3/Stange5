77e9eb07426a30fe5be3b99146974d45f49d39a4
vrlx3 <bhavsar.viral@outlook.com>
2021-06-01 12:57:31 -0500

    noon push

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
-import React from 'react'
+import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 import {Header} from './components'
 
@@ -7,7 +7,6 @@ import {Header} from './components'
 const BASE = 'https://strangers-things.herokuapp.com/api/2104-UIC-RM-WEB-FT/'
 
 
-const TOKEN = '' // Some token
 
 
 // axios.get(BASE)
@@ -23,26 +22,63 @@ const TOKEN = '' // Some token
 //     console.log(error);
 // })
 
-await axios.post('https://strangers-things.herokuapp.com/api/2104-UIC-RM-WEB-FT/users/register', {
+// await axios.post('https://strangers-things.herokuapp.com/api/2104-UIC-RM-WEB-FT/users/register', {
+   
+//       uRegister,
+//     {
+//         headers: {
+//             Content-Type :'application/json' 
+//         },
+
+//         body: {
+//             user: {
+//                 username: 'twenty',
+//                 password: 'dollar'
+//             }
+//         }
+
+//     } 
+// }
+// "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MGI2NTkzY2VkODRkNjAwMTcxYWJiZDIiLCJ1c2VybmFtZSI6InR3ZW50Ymxrc2J2a2pzeXZsYnZsZGZudmxnaGt1ZGZoIiwiaWF0IjoxNjIyNTYzMTMyfQ.O3crL9IJRzXNlVrw01vxfOErnlhrtmar3PGV76DgXOo"
 
 
-    uRegister,
-    {
-        headers: {
-            Content-Type:'application/json' 
-        }
-
-    } 
+ 
 
 
 
 const App = () => {
+
+    
+    const [token, setToken] = useState()
+
+    function logUser () {fetch('https://strangers-things.herokuapp.com/api/2104-UIC-RM-WEB-FT/users/login', {
+        method: "POST",
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+        user: {
+        username: 'twentblksbvkjsyvlbvldfnvlghkudfh',
+        password: 'dollar'
+        }
+        })
+    }).then(response => response.json())
+    .then(result => {
+    console.log(result.data.token);
+    setToken(result.data.token)
+    })
+    .catch(console.error);
+    }
+    
+    console.log(token)
     return (
     <div id='app'>
         <Header/>
+        
     
         <div id='page'>
             <h1>new world</h1>
+            <button onClick={logUser}>login</button>
             <h2>This is the new world</h2>
         </div>
     </div>
