43be9299fcf75e0de6af7a7edf44b7059f258ecd
vrlx3 <bhavsar.viral@outlook.com>
2021-06-01 13:32:04 -0500

    made reg form

---
+++ src/components/reguser.js
@@ -0,0 +1,29 @@
+
+const newuser = () => {
+    //extract info from form below and run 
+
+    // username & password
+
+
+
+    // update token variable
+    // Welcome mr. superman
+
+    console.log('new user register attempt')
+}
+
+const Reguser = () => {
+
+    return (<div id='reguser'>
+            <input type='form' placeholder='Choose Username'></input>
+            <p></p>
+            <input type='form' placeholder='Choose Password'></input>
+            <button onClick={newuser}>Register</button>
+            
+
+            </div>
+
+    )
+}
+
+export default Reguser;
--- src/index.js
+++ src/index.js
@@ -1,44 +1,13 @@
 import axios from 'axios'
 import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
-import {Header} from './components'
+import {Header, Reguser} from './components'
+
 
 
 const BASE = 'https://strangers-things.herokuapp.com/api/2104-UIC-RM-WEB-FT/'
 
 
-
-
-// axios.get(BASE)
-// .then(function (response) {
-//     const data = response.data.data;
-//     const posts = data.posts;
-//     console.log('post', posts)
-//     console.log(posts[0])
-//     console.log(posts[0].description)
-
-// })
-// .catch(function (error) {
-//     console.log(error);
-// })
-
-// await axios.post('https://strangers-things.herokuapp.com/api/2104-UIC-RM-WEB-FT/users/register', {
-   
-//       uRegister,
-//     {
-//         headers: {
-//             Content-Type :'application/json' 
-//         },
-
-//         body: {
-//             user: {
-//                 username: 'twenty',
-//                 password: 'dollar'
-//             }
-//         }
-
-//     } 
-// }
 // "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MGI2NTkzY2VkODRkNjAwMTcxYWJiZDIiLCJ1c2VybmFtZSI6InR3ZW50Ymxrc2J2a2pzeXZsYnZsZGZudmxnaGt1ZGZoIiwiaWF0IjoxNjIyNTYzMTMyfQ.O3crL9IJRzXNlVrw01vxfOErnlhrtmar3PGV76DgXOo"
 
 
@@ -81,6 +50,8 @@ const App = () => {
             <button onClick={logUser}>login</button>
             <h2>This is the new world</h2>
         </div>
+
+        <Reguser/>
     </div>
     )
 }
