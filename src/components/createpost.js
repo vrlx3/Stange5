baca8dbadf807579c417e9de5fd39292dc4da318
vrlx3 <bhavsar.viral@outlook.com>
2021-06-02 10:30:28 -0500

    create post

---
+++ src/components/createpost.js
@@ -0,0 +1,50 @@
+import React, {useState} from 'react';
+
+const Create = () => {
+    const [title, setTitle] = useState([])
+    const [description, setDescription] = useState([])
+
+    const handleSubmit = (ev) => {
+        ev.preventDefault();
+        console.log('title, description ', title, description);
+
+        const response = await fetch(curl, {
+                                             method: 'POST',
+                                             header: {
+                                                        'Content-Type': 'application/json',
+                                                        'Authorization': Bearer {token}               
+                                                        },
+                                            body: JSON.stringify({
+                                                                    title,
+                                                                    description,                        
+                                                                    })   
+                                            })
+        const data = await response.json();
+        console.log('data ', data);
+        // setPosts([data,...posts])
+        setTitle('')
+        setDescription('')
+    
+    }
+
+    return <>
+            <h3>Create a Post</h3>
+            <form onSubmit={handleSubmit}>
+                <input 
+                    type="text"
+                    placeholder="Title"
+                    value="title"
+                    onChange={(ev) => setTitle(ev.target.value)}            
+                    />
+                <input
+                    type="text"
+                    placeholder="Description"
+                    value="description"
+                    onChange={(ev) => setDescription(ev.target.value)}
+                    />
+            </form>
+            </>
+
+}
+
+export default Create;
--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,4 @@
 export {default as Header} from './header'
 export {default as Reguser} from './reguser'
 export {default as RenderPosts} from './renderPosts'
+export {default as Create} from './createpost'
